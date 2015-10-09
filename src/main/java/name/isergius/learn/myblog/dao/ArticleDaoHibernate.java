fa8042ff793887b078ecb3a5e068bce9eb29b860
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-09 07:16:40 +0500

    Implement method readBy(long id)

--- src/main/java/name/isergius/learn/myblog/dao/ArticleDaoHibernate.java
+++ src/main/java/name/isergius/learn/myblog/dao/ArticleDaoHibernate.java
@@ -1,14 +1,25 @@
 package name.isergius.learn.myblog.dao;
 
 import name.isergius.learn.myblog.domain.Article;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 /**
  * Created by Kondratyev Sergey on 05.10.15.
  */
 public class ArticleDaoHibernate {
 
+    private SessionFactory sessionFactory;
+
+    public ArticleDaoHibernate(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     public Article readBy(long id) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Article result = session.get(Article.class, id);
+        session.close();
+        return result;
     }
 
 }
