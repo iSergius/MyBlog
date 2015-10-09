1ae492a809715f87504a9c42f3635c9336eec808
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-10 02:21:22 +0500

    create test on method create( Article article)

--- src/test/java/name/isergius/learn/myblog/dao/ArticleDaoTest.java
+++ src/test/java/name/isergius/learn/myblog/dao/ArticleDaoTest.java
@@ -3,6 +3,7 @@ package name.isergius.learn.myblog.dao;
 import name.isergius.learn.myblog.domain.Article;
 import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.dbunit.annotation.ExpectedDataSet;
 import org.unitils.spring.annotation.SpringBean;
 
 /**
@@ -21,4 +22,11 @@ public class ArticleDaoTest extends AbstractDbTest {
         assertEquals("My Firs Article", article.getTitle());
     }
 
+    @Test
+    @ExpectedDataSet
+    public void testCreateWithSetId() throws Exception {
+        Article article = new Article("My Second Article");
+        article = dao.create(article);
+        assertNotNull(article.getId());
+    }
 }
