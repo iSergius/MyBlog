8b6b5d66eef40e5af1a1d9dfd01031137e392ba2
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-11 07:02:35 +0500

    Add constructors in DaoException

--- src/main/java/name/isergius/learn/myblog/dao/DaoException.java
+++ src/main/java/name/isergius/learn/myblog/dao/DaoException.java
@@ -5,4 +5,15 @@ package name.isergius.learn.myblog.dao;
  */
 public class DaoException extends Exception {
 
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
 }
