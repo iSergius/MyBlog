6c4934290d2233acc73c08287ecd21359cf8a4e6
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-11 05:51:45 +0500

    implement test case: update is not contain in database entity

--- src/main/java/name/isergius/learn/myblog/dao/ArticleDaoHibernate.java
+++ src/main/java/name/isergius/learn/myblog/dao/ArticleDaoHibernate.java
@@ -3,6 +3,7 @@ package name.isergius.learn.myblog.dao;
 import name.isergius.learn.myblog.domain.Article;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Created by Kondratyev Sergey on 05.10.15.
@@ -31,12 +32,27 @@ public class ArticleDaoHibernate {
         return article;
     }
 
-    public void update(Article article) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.update(article);
-        session.getTransaction().commit();
-        session.close();
+    public void update(Article article) throws DaoException {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+
+            session.update(article);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DaoException();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     public void deleteBy(long id) {
