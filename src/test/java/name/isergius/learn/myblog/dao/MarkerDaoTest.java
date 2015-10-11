868a6e0865166bb237d49ac489f75b20abc53fbf
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-12 04:13:23 +0500

    Create test class on entity Marker create test on case read entity by id

---
+++ src/test/java/name/isergius/learn/myblog/dao/MarkerDaoTest.java
@@ -0,0 +1,24 @@
+package name.isergius.learn.myblog.dao;
+
+import name.isergius.learn.myblog.domain.Marker;
+import org.junit.Test;
+import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.spring.annotation.SpringBean;
+
+/**
+ * Created by Kondratyev Sergey on 11.10.15.
+ */
+
+public class MarkerDaoTest extends AbstractDbTest {
+
+    @SpringBean("markerDao")
+    private MarkerDabHibernate dao;
+
+    @Test
+    @DataSet
+    public void testReadById() throws DaoException {
+        Marker marker = dao.readBy(1L);
+        assertEquals("My Firs Marker", marker.getTitle());
+    }
+
+}
