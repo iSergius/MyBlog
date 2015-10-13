bc159b7ccbf31fb1d4d8c2f3291d97c35c1af9ce
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-13 10:05:42 +0500

    Implement method readPublishedBy(Long id, boolean published)

--- src/main/java/name/isergius/learn/myblog/dao/hibernate/ArticleDaoHibernate.java
+++ src/main/java/name/isergius/learn/myblog/dao/hibernate/ArticleDaoHibernate.java
@@ -2,7 +2,9 @@ package name.isergius.learn.myblog.dao.hibernate;
 
 import name.isergius.learn.myblog.dao.ArticleDao;
 import name.isergius.learn.myblog.domain.Article;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Created by Kondratyev Sergey on 05.10.15.
@@ -13,4 +15,30 @@ public class ArticleDaoHibernate extends AbstractDaoHibernate<Article> implement
         super(sessionFactory,Article.class);
     }
 
+    @Override
+    public Article readPublishedBy(long id, boolean published) {
+        Session session = null;
+        Transaction transaction = null;
+        Article result = null;
+
+        try {
+            session = getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            result = (Article) session.createQuery("from Article a where a.published = :published")
+                                    .setBoolean("published", published)
+                                    .uniqueResult();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return result;
+    }
 }
