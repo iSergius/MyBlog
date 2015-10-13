c3399264f59b2380a9f45fe92b8663c511233c7f
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-13 10:03:26 +0500

    Add in Interface ArticleDao method readPublishedBy(Long id, boolean published)

--- src/main/java/name/isergius/learn/myblog/dao/ArticleDao.java
+++ src/main/java/name/isergius/learn/myblog/dao/ArticleDao.java
@@ -6,4 +6,7 @@ import name.isergius.learn.myblog.domain.Article;
  * Created by Kondratyev Sergey on 12.10.15.
  */
 public interface ArticleDao extends Dao<Article> {
+
+    Article readPublishedBy(long id, boolean published);
+
 }
