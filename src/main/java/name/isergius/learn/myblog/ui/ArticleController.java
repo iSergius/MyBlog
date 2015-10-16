4af0cd5c45d6d33c6b499fd3695c3ebf9c1c0b56
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-16 19:23:04 +0500

    Implementing test case getting Article

--- src/main/java/name/isergius/learn/myblog/ui/ArticleController.java
+++ src/main/java/name/isergius/learn/myblog/ui/ArticleController.java
@@ -1,5 +1,8 @@
 package name.isergius.learn.myblog.ui;
 
+import name.isergius.learn.myblog.domain.Note;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +16,14 @@ public class ArticleController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String pathInfo = request.getPathInfo();
+
+        Integer articleId = Integer.valueOf(pathInfo.substring(1));
+        ServletContext servletContext = request.getServletContext();
+        Note note = (Note) servletContext.getAttribute("note");
+        note.getPublishedArticleBy(articleId);
+
         request.getRequestDispatcher("article.jsp").forward(request,response);
     }
 
