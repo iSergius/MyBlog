7c659d0ec2bc2037a6723e12b2d71150489befa4
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-17 09:12:50 +0500

    implement check value Article id

--- src/main/java/name/isergius/learn/myblog/ui/ArticleController.java
+++ src/main/java/name/isergius/learn/myblog/ui/ArticleController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Created by Kondratyev Sergey on 16.10.15.
@@ -18,7 +19,10 @@ public class ArticleController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String pathInfo = request.getPathInfo();
-
+        if (!Pattern.matches("/\\d+",pathInfo)) {
+            response.sendError(404);
+            return;
+        }
         Integer articleId = Integer.valueOf(pathInfo.substring(1));
         ServletContext servletContext = request.getServletContext();
         Note note = (Note) servletContext.getAttribute("note");
