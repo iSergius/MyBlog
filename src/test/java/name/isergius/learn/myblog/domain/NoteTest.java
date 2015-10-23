22e6da9a7a32dfe37046798e5c5d84056f51887d
Kondratyev Sergey <ron.rcf@gmail.com>
2015-10-23 13:53:34 +0500

    Rewrite NoteTest on Mockito framework change test spring context remove old config files

--- src/test/java/name/isergius/learn/myblog/domain/NoteTest.java
+++ src/test/java/name/isergius/learn/myblog/domain/NoteTest.java
@@ -1,35 +1,66 @@
 package name.isergius.learn.myblog.domain;
 
+import name.isergius.learn.myblog.dao.ArticleDao;
+import name.isergius.learn.myblog.dao.MarkerDao;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
 /**
  * Created by Kondratyev Sergey on 13.10.15.
  */
-public class NoteTest extends AbstractSpringContext {
+@RunWith(MockitoJUnitRunner.class)
+public class NoteTest extends Assert {
 
-    @Autowired
-    @Qualifier("note")
-    private Note note;
+    @Mock
+    private ArticleDao articleDao;
+    @Mock
+    private MarkerDao markerDao;
+    @Mock
+    private Article article;
+    @Mock
+    private List<Article> articles;
+    @Mock
+    private List<Marker> markers;
+
+    private Note note = null;
+
+    @Before
+    public void setUp() throws Exception {
+        note = new Note(articleDao,markerDao);
+    }
 
     @Test
     public void testGetPublishedArticleBy() throws Exception {
+        Mockito.when(articleDao.readBy(1L,true)).thenReturn(article);
+        Mockito.when(article.getPublished()).thenReturn(true);
+
         Article article = note.getPublishedArticleBy(1L);
+
         assertTrue(article.getPublished());
     }
 
     @Test
     public void testGetAllPublishedMarkers() throws Exception {
+        Mockito.when(markerDao.readAll(true)).thenReturn(markers);
+
         List<Marker> markers = note.getAllPublishedMarkers();
+
         assertNotNull(markers);
     }
 
     @Test
     public void testGettingAllPublishedArticles() throws Exception {
+        Mockito.when(articleDao.readAll(true)).thenReturn(articles);
+
         List<Article> articles = note.getAllPublishedArticles();
+
         assertNotNull(articles);
     }
 }
